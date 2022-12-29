8bbf2ba466fe8a206153395dae79266ef88228c2
stream1080 <fmxhax638@foxmail.com>
2022-12-29 21:52:21 +0800

    feat: 定义连接模块的接口

---
+++ ziface/connect.go
@@ -0,0 +1,16 @@
+package ziface
+
+import "net"
+
+// 连接模块的接口
+type Connect interface {
+	Start()                      // 启动连接
+	Stop()                       // 关闭连接
+	GetTCPConnect() *net.TCPConn // 获取当前连接绑定的 socket conn
+	GetConnId() uint32           // 获取当前连接的 Id
+	RemoteAddr() *net.Addr       // 获取远程客户端的 TCP 状态 Ip，Port
+	Send(data []byte) error      // 发送数据
+}
+
+// 处理连接业务的方法
+type HandleFunc func(*net.TCPConn, []byte, int) error
