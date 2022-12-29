a1608f51e5323adbb6dfc986c8cc43b582b944ba
stream1080 <fmxhax638@foxmail.com>
2022-12-29 22:00:31 +0800

    fix: 修改 RemoteAddr 返回值错误

--- ziface/connect.go
+++ ziface/connect.go
@@ -8,7 +8,7 @@ type Connect interface {
 	Stop()                       // 关闭连接
 	GetTCPConnect() *net.TCPConn // 获取当前连接绑定的 socket conn
 	GetConnId() uint32           // 获取当前连接的 Id
-	RemoteAddr() *net.Addr       // 获取远程客户端的 TCP 状态 Ip，Port
+	RemoteAddr() net.Addr        // 获取远程客户端的 TCP 状态 Ip，Port
 	Send(data []byte) error      // 发送数据
 }
 
