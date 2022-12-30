399c11a38dafab9e72bb1332ea6f92b935f01d35
stream1080 <fmxhax638@foxmail.com>
2022-12-30 23:08:18 +0800

    feat: 增加 router 模块

---
+++ znet/router.go
@@ -0,0 +1,19 @@
+package znet
+
+import "github.com/stream1080/zinx/ziface"
+
+// 实现 router 时，先嵌入这个基类，然后根据需要对这个基类的方法进行重写
+type BaseRouter struct{}
+
+// 这里之所以 BaseRouter 的方法都为空，
+// 是因为有的 Router 不希望有所有方法都实现
+// 所以 Router 全部继承 BaseRouter 的好处是，不需要实现全部方法也可以实例化
+
+// 处理业务前的钩子方法 Hook
+func (b *BaseRouter) PreHandle(request ziface.Request) {}
+
+// 处理业务的主方法 Hook
+func (b *BaseRouter) Handle(request ziface.Request) {}
+
+// 处理业务后的钩子方法 Hook
+func (b *BaseRouter) PostHandle(request ziface.Request) {}
