2904143f0ec2a0dcca34409248260fc91f448237
stream1080 <fmxhax638@foxmail.com>
2023-01-02 10:25:04 +0800

    feat: server 集成连接管理器

--- znet/server.go
+++ znet/server.go
@@ -10,11 +10,12 @@ import (
 
 // Server 的服务接口实现
 type Server struct {
-	Name      string         // 名称
-	IpVersion string         // ip版本
-	IP        string         // ip地址
-	Port      int            // 端口
-	msgHandle face.MsgHandle // 消息管理器
+	Name      string           // 名称
+	IpVersion string           // ip版本
+	IP        string           // ip地址
+	Port      int              // 端口
+	msgHandle face.MsgHandle   // 消息管理器
+	ConnMgr   face.ConnManager // 连接管理器
 }
 
 // 初始化 Server 方法
@@ -25,6 +26,7 @@ func NewServer() face.Server {
 		IP:        conf.ServerConfig.Host,
 		Port:      conf.ServerConfig.Port,
 		msgHandle: NewMsgHandle(),
+		ConnMgr:   NewConnManager(),
 	}
 }
 
@@ -64,6 +66,12 @@ func (s *Server) Start() {
 				continue
 			}
 
+			// 是否大于最大连接数
+			if s.ConnMgr.Count() >= conf.ServerConfig.MaxConn {
+				conn.Close()
+				continue
+			}
+
 			// 将处理连接到业务方法与 conn 进行绑定
 			dealConn := NewConnect(conn, connId, s.msgHandle)
 			connId++
@@ -92,4 +100,9 @@ func (s *Server) Serve() {
 func (s *Server) AddRouter(msgId uint32, router face.Router) {
 	s.msgHandle.AddRouter(msgId, router)
 	fmt.Println("Add Router Success! ")
+}
+
+// 获取连接管理器
+func (s *Server) GetConnMgr() face.ConnManager {
+	return s.ConnMgr
 }
