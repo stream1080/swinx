0be1824132d90a5f25a9330fcad2411eb23ac9f8
stream1080 <fmxhax638@foxmail.com>
2023-01-03 22:10:12 +0800

    style: 修改文件名

--- face/msg_handle.go
+++ face/msg_handle.go
@@ -5,5 +5,5 @@ type IMsgHandle interface {
 	DoMsgHandler(request IRequest)          // 以非阻塞方式处理消息
 	AddRouter(msgId uint32, router IRouter) // 为消息添加具体处理逻辑
 	StartWorkerPool()                       // 启动 worker 工作池
-	SendMsgTaskQueue(request IRequest)      // 将消息交给 TaskQueue ,由 worker 进行处理
+	SendMsgTaskQueue(request IRequest)      // 将消息交给 TaskQueue , 由 worker 进行处理
 }
