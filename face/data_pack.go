18f341417a1f60845a39b63fd8c10e560c1e7525
stream1080 <fmxhax638@foxmail.com>
2023-01-27 17:57:29 +0800

    feat: 增加封包拆包的 factory

--- face/data_pack.go
+++ face/data_pack.go
@@ -6,3 +6,11 @@ type IDataPack interface {
 	Pack(msg IMessage) ([]byte, error) // 封包方法
 	UnPack([]byte) (IMessage, error)   // 拆包方法
 }
+
+const (
+	SwinxDataPack  string = "swinx_pack"  // Swinx 标准封包和拆包方式
+	CustomDataPack string = "custom_pack" // 自定义封包方式
+
+	SwinxMessage  string = "swinx_message"  // Swinx 默认标准报文协议格式
+	CustomMessage string = "custom_message" // 自定义报文协议格式
+)
